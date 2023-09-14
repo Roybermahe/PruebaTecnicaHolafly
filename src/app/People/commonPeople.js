6ff33384ade8882fabacf1886a4963d892cd6c16
Roybermahe <Roybermahe@github.com>
2023-09-14 08:29:22 -0500

    fix (CommonPeople): Se coloca module.exports y se agrega super()

--- src/app/People/commonPeople.js
+++ src/app/People/commonPeople.js
@@ -2,6 +2,9 @@ const AbstractPeople = require("./abstractPeople");
 
 class CommonPeople extends AbstractPeople {
     constructor(id){
+       super(id);
        this.id = id;
     }
 }
+
+module.exports = CommonPeople;
