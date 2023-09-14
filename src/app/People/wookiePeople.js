40d550a1f7b5f824504116fe9e2388793fa18d3f
Roybermahe <Roybermahe@github.com>
2023-09-14 11:34:52 -0500

    feat (WookieePeople): Se agrega la clase WooKiePeople para el manejo del lenguaje wookie para people

---
+++ src/app/People/wookiePeople.js
@@ -0,0 +1,24 @@
+const AbstractPeople = require("./abstractPeople");
+const db = require('../db');
+const app = require('..');
+const format = '/?format=wookiee'
+class WookiePeople  extends AbstractPeople {
+    constructor(id){
+        super(id);
+        this.id = id;
+    }
+
+    async init(){
+        const peopleSwapi = await app.swapiFunctions.genericRequest('https://swapi.dev/api/people/'+this.id+format, 'GET', null, true );
+        const urlPlanet = peopleSwapi.acooscwoohoorcanwa.replace('acaoaoakc://cohraakah.wawoho/raakah/akanrawhwoaoc/', 'https://swapi.dev/api/planets/')
+        const planetSwapi = await app.swapiFunctions.genericRequest(urlPlanet + format, 'GET', null, true );
+        const { scracc, acwoahrracao, whrascwo } = peopleSwapi;
+        this.mass = scracc;
+        this.height = acwoahrracao;
+        this.name = whrascwo;
+        this.homeworlId= peopleSwapi.acooscwoohoorcanwa.split('acaoaoakc://cohraakah.wawoho/raakah')[1];
+        this.homeworldName = planetSwapi.whrascwo;
+    }
+}
+
+module.exports = WookiePeople
